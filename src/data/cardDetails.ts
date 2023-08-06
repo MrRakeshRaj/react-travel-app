c583ed586086f7037c5cf0156020fbbd02582d39
rocky <cgpa10.0@gmail.com>
2023-08-07 01:31:45 +0530

    modify path of images

--- src/data/cardDetails.ts
+++ src/data/cardDetails.ts
@@ -2,16 +2,16 @@ export const cardDetails = [
   {
     title: "Surfing",
     content: "Best Hawaiian islands for surfing.",
-    imgPath: "src/assets/surfing.jpg",
+    imgPath: "./src/assets/surfing.jpg",
   },
   {
     title: "Hula",
     content: "Try it yourself.",
-    imgPath: "src/assets/hula.jpg",
+    imgPath: "./src/assets/hula.jpg",
   },
   {
     title: "Vulcanoes",
     content: "Volcanic conditions can change at any time.",
-    imgPath: "src/assets/volcano.jpg",
+    imgPath: "./src/assets/volcano.jpg",
   },
 ];
