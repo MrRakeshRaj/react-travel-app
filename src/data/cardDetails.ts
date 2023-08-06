db96cd1801ab194847f74e13793ef8e9e129c2ac
rocky <cgpa10.0@gmail.com>
2023-08-07 01:59:40 +0530

    update image path

--- src/data/cardDetails.ts
+++ src/data/cardDetails.ts
@@ -2,16 +2,19 @@ export const cardDetails = [
   {
     title: "Surfing",
     content: "Best Hawaiian islands for surfing.",
-    imgPath: "./src/assets/surfing.jpg",
+    imgPath:
+      "https://unsplash.com/photos/stj-A6E6II8/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NTF8fHN1cmZpbmd8ZW58MHx8fHwxNjkxMzUwMDExfDA&force=true&w=640",
   },
   {
     title: "Hula",
     content: "Try it yourself.",
-    imgPath: "./src/assets/hula.jpg",
+    imgPath:
+      "https://unsplash.com/photos/AxbvZI8RfYo/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8aHVsYSUyMGhhd2FpaXxlbnwwfHx8fDE2OTEzMDYyNDV8MA&force=true&w=640",
   },
   {
     title: "Vulcanoes",
     content: "Volcanic conditions can change at any time.",
-    imgPath: "./src/assets/volcano.jpg",
+    imgPath:
+      "https://unsplash.com/photos/f_knaWveAGk/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjkxMzUzNjM3fA&force=true&w=1920",
   },
 ];
