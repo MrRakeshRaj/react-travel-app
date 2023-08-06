7a65b2b2a2fddd74ede976ffd1b982dda0d4260e
rocky <cgpa10.0@gmail.com>
2023-08-06 22:02:39 +0530

    clean code details component

--- src/components/details-section/Categories.tsx
+++ src/components/details-section/Categories.tsx
@@ -1,3 +1,27 @@
+import { Headings } from "../Headings";
+import { categories } from "../../data/categories";
+import { ArrowButton } from "../highlights-section/ArrowButton";
+
 export function Categories(): JSX.Element {
-  return <>This is categories</>;
+  return (
+    <>
+      <div className="flex flex-col w-[544px] mt-10 ml-24">
+        <Headings title={"Categories"} />
+
+        <div className="flex flex-col mt-2 p-2 gap-2">
+          {categories.map((item, i) => (
+            <div
+              key={i}
+              className="bg-white flex flex-row font-robotomono font-light text-sm justify-between items-center text-black max-w-md rounded-xl w-[544px] h-[68px]"
+            >
+              <div className="ml-2 pl-3">{item.title}</div>
+              <div className="m1-2 pr-2">
+                <ArrowButton bg={false} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/details-section/TravelGuide.tsx
+++ src/components/details-section/TravelGuide.tsx
@@ -1,3 +1,34 @@
+import { travelGuideDetails } from "../../data/travelGuide";
+import { TravelGuideImage } from "./TravelGuideImage";
+import { Headings } from "../Headings";
+
 export function TravelGuide(): JSX.Element {
-  return <>This is Travel Guide Details</>;
+  return (
+    <>
+      <div className="relative right-16 flex flex-col w-[560px] mt-10 mr-10">
+        <Headings title={"Travel Guide"} />
+        <div className="flex flex-row justify-between items-center w-full h-[190px] bg-white mt-4 rounded-lg">
+          <div className="flex flex-col mr-4">
+            <div className="pl-6 pr-6 font-robotomono">
+              <h5 className="text-2xl -mt-5 font-semibold leading-tight text-black ">
+                {travelGuideDetails[0]?.name}
+              </h5>
+              <p className="mb-10 text-lg text-black">
+                {travelGuideDetails[0]?.content}
+              </p>
+              <button
+                type="button"
+                className="font-bold font-robotomono text-lg text-teal-700 border border-teal-700 items-center p-1 pr-4 pl-4  focus:outline-none rounded-md hover:border hover:bg-teal-700 hover:text-white focus:z-10 focus:ring-4 focus:ring-teal-700"
+              >
+                Contact
+              </button>
+            </div>
+          </div>
+          <div className="mr-3 pr-3">
+            <TravelGuideImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
